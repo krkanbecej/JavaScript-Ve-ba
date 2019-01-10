3991ee04873b6c4caa72b1de7d68a99be2f4e965
Aleksandar Karapandza <40364953+krkanbecej@users.noreply.github.com>
2019-01-10 05:16:49 +0100

    prvi komit

---
+++ index.js
@@ -0,0 +1,25 @@
+// ovo je komentar u JS
+let name ='Krka';
+console.log(name);
+
+// ne koristite ključne reči
+// koristite smislene nazive
+// ne započinjite nazive brojevima
+// ne koristite razmak i crticu(-)
+// nazivi su casesenzitiv odnosno prepoznaju velika i mala slova
+// let firstName = 'Aleksandar';
+// let lastName = 'Karapandza';
+
+let = rezultatRacunanja = 0,56;
+rezultatRacunanja = 5.5-3;
+console.log(rezultatRacunanja);
+
+// const = rezultatRacunanja = 0,56;
+// rezultatRacunanja = 5;
+// console.log(rezultatRacunanja);
+
+let ime ='Krka'; //string 
+let age = 53;//namber literal
+let isApruved = false;//boolen literal
+let firstName = undefined;
+let selektColor = null;
